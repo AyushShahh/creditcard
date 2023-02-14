eb1534def44b0073d2458c37cc08a8d7c1768c3a
Ayush Shah <71313814+AyushShahh@users.noreply.github.com>
2023-02-15 01:00:35 +0530

    remove count parameter from prefix

--- digit.h
+++ digit.h
@@ -5,6 +5,6 @@ double pow(double n, double p);
 int digit_count(long n);
 int digit_extractor(long n, int digit);
 long get_long(char *text);
-int prefix(long n, int start, int count);
+int prefix(long n, int start);
 
 #endif
